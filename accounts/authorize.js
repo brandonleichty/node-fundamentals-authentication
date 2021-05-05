ba32513940de42d02afc6bd62724441603f6a7aa
Brandon Leichty <brandonl@me.com>
2021-05-04 22:43:34 -0500

    add authorizeUser functionality (compare password vs hashed password)

--- accounts/authorize.js
+++ accounts/authorize.js
@@ -1,3 +1,20 @@
-export async function authorizeUser(email, password) {
+import bcrypt from 'bcryptjs'
 
+const {compare} = bcrypt;
+
+export async function authorizeUser(email, password) {
+  // Import User collectoin 
+  const { user } = await import("../user/user.js");
+
+  // Look up user
+  const userData = await user.findOne({'email.address': email})
+  console.log("userData", userData)
+  // Get user password
+  const savedPassword = userData.password;
+
+  // Compare password with one in database
+  const isAuthorized = await compare(password, savedPassword)
+  // Return a boolean if password is correct: true / false
+  
+  return isAuthorized;
 }
